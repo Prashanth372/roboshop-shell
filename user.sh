410a28e37f35d9515c35153ff979cede06740d01
Prashanth <mprashanth372@gmail.com>
2024-03-07 20:22:28 +0530

    Added Redis

---
+++ user.sh
@@ -0,0 +1,94 @@
+#!/bin/bash
+
+DATE=$(date +%F)
+LOGSDIR=/tmp
+# /home/centos/shellscript-logs/script-name-date.log
+SCRIPT_NAME=$0
+LOGFILE=$LOGSDIR/$0-$DATE.log
+USERID=$(id -u)
+R="\e[31m"
+G="\e[32m"
+N="\e[0m"
+Y="\e[33m"
+
+if [ $USERID -ne 0 ];
+then    
+    echo -e "$R ERROR:: Please run this scirpt with root access $N"
+    exit 1
+fi
+
+VALIDATE(){
+    if [ $1 -ne 0 ];
+    then
+        echo -e "$2 ... $R FAILURE $N"
+        exit 1
+    else
+        echo -e "$2 ... $G SUCCESS $N"
+    fi
+}    
+
+dnf module disable nodejs -y &>>$LOGFILE
+VALIDATE $? "Module disabled nodeJS"
+
+dnf module enable nodejs:18 -y &>>$LOGFILE
+VALIDATE $? "Enable NodeJS 18 version"
+
+dnf install nodejs -y  &>>$LOGFILE
+VALIDATE $? "Installing NodeJS"
+
+
+#Once user is created, if you run this script 2nd time then it will be fail
+#this command will defintely fail
+# IMPROVEMENT : Firest check the user already exist or not, if not exist then create
+useradd roboshop &>>$LOGFILE
+
+#write a condition to check if directory already exist or not
+mkdir /app &>>$LOGFILE
+
+curl -o /tmp/user.zip https://roboshop-builds.s3.amazonaws.com/user.zip &>>$LOGFILE
+
+VALIDATE $? "Downloading user artifact"
+
+cd /app &>>$LOGFILE
+
+VALIDATE $? "Moving to app directory"
+
+unzip /tmp/user.zip &>>$LOGFILE
+
+VALIDATE $? "Unzipping user"
+
+cd /app &>>$LOGFILE
+
+npm install &>>$LOGFILE
+
+VALIDATE $? "Installing dependencies"
+
+#Give full path of user.service as we are inside /app
+cp /home/centos/roboshop-shell/user.service /etc/systemd/system/user.service &>>$LOGFILE
+
+VALIDATE $? "Copying user.service"
+
+systemctl daemon-reload &>>$LOGFILE
+
+VALIDATE $? "daemon-reload"
+
+systemctl enable user &>>$LOGFILE
+
+VALIDATE $? "enabling user"
+
+systemctl start user &>>$LOGFILE
+
+VALIDATE $? "starting user"
+
+cp /home/centos/roboshop-shell/mongo.repo /etc/yum.repos.d/mongo.repo &>>$LOGFILE
+
+VALIDATE $? "Copying mongo.repo"
+
+dnf install mongodb-org-shell -y &>>$LOGFILE
+
+VALIDATE $? "Installing Mongo Client"
+
+mongo --host mongodb.suvarnalaxmiinfradevelopers.online </app/schema/user.js &>>$LOGFILE
+
+VALIDATE $? "Loading user data into mongoDB"
+
