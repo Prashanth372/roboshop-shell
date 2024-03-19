bf7d385f5b0643483d83d60050e47e60a77cc330
Prashanth <mprashanth372@gmail.com>
2024-03-19 23:30:25 +0530

    Added cart

--- cart.sh
+++ cart.sh
@@ -40,7 +40,7 @@ VALIDATE $? "Installing NodeJS"
 #Once user is created, if you run this script 2nd time then it will be fail
 #this command will defintely fail
 # IMPROVEMENT : Firest check the user already exist or not, if not exist then create
-useradd roboshop &>>$LOGFILE
+##########useradd roboshop &>>$LOGFILE
 
 #write a condition to check if directory already exist or not
 ########mkdir /app &>>$LOGFILE
