8032354e333977a1df60de9731f348944b3b38de
Prashanth <mprashanth372@gmail.com>
2024-03-19 23:29:34 +0530

    Added cart

--- cart.sh
+++ cart.sh
@@ -43,9 +43,9 @@ VALIDATE $? "Installing NodeJS"
 useradd roboshop &>>$LOGFILE
 
 #write a condition to check if directory already exist or not
-mkdir /app &>>$LOGFILE
+########mkdir /app &>>$LOGFILE
 
-curl -o /tmp/cart.zip https://roboshop-builds.s3.amazonaws.com/cart.zip &>>$LOGFILE
+##########curl -o /tmp/cart.zip https://roboshop-builds.s3.amazonaws.com/cart.zip &>>$LOGFILE
 
 VALIDATE $? "Downloading cart artifact"
 
@@ -53,7 +53,7 @@ cd /app &>>$LOGFILE
 
 VALIDATE $? "Moving to app directory"
 
-unzip /tmp/cart.zip &>>$LOGFILE
+#######unzip /tmp/cart.zip &>>$LOGFILE
 
 VALIDATE $? "Unzipping cart"
 
