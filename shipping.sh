af9245aac047226faf367f0bf62c8b1a885816d9
Prashanth <mprashanth372@gmail.com>
2024-03-21 14:49:52 +0530

    added shipping

--- shipping.sh
+++ shipping.sh
@@ -26,3 +26,66 @@ VALIDATE(){
         echo -e "$2 ... $G SUCCESS $N"
     fi
 }    
+
+
+dnf install maven -y &>> $LOGFILE
+
+VALIDATE $? "Installing Maven"
+
+useradd roboshop &>> $LOGFILE
+
+VALIDATE $? "User added"
+
+mkdir /app
+
+VALIDATE $? "Creating app directory"
+
+curl -L -o /tmp/shipping.zip https://roboshop-builds.s3.amazonaws.com/shipping.zip &>> $LOGFILE
+
+VALIDATE $? "Downloading shipping artifact"
+
+unzip /tmp/shipping.zip &>> $LOGFILE
+
+VALIDATE $? "Unzipping shipping artifact"
+
+cd /app &>> $LOGFILE
+
+VALIDATE $? "Moving to app directory"
+
+mvn clean package &>> $LOGFILE
+
+VALIDATE $? "Packaging shipping app"
+
+mv target/shipping-1.0.jar shipping.jar &>> $LOGFILE
+
+VALIDATE $? "renaming shipping jar"
+
+cp /home/centos/roboshop-shell/shipping.service /etc/systemd/system/shipping.service &>> $LOGFILE
+
+VALIDATE $? "copying shipping.service"
+
+systemctl daemon-reload &>> $LOGFILE
+
+VALIDATE $? "daemon-reload"
+
+systemctl enable shipping &>> $LOGFILE
+
+VALIDATE $? "enabling shipping"
+
+systemctl start shipping &>> $LOGFILE
+
+VALIDATE $? "starting shipping"
+
+dnf install mysql -y &>> $LOGFILE
+
+VALIDATE $? "Installing mysql client"
+
+mysql -h mysql.suvarnalaxmiinfradevelopers.online -uroot -pRoboShop@1 < /app/schema/shipping.sql &>> $LOGFILE
+
+VALIDATE $? "Loaded countries and cities"
+
+systemctl restart shipping &>> $LOGFILE
+
+VALIDATE $? "restarting shipping"
+
+
