d7cfb85f0650632c756d2f3c16677e1cfa5cab29
Prashanth <mprashanth372@gmail.com>
2024-03-21 12:46:04 +0530

    added mysql

---
+++ mysql.sh
@@ -0,0 +1,55 @@
+#!/bin/bash
+
+DATE=$(date +%F)
+LOGSDIR=/tmp
+# /home/centos/shellscript-logs/script-name-date.log
+SCRIPT_NAME=$0
+LOGFILE=$LOGSDIR/$0-$DATE.log
+USERID=$(id -u)
+R="\e[31m"
+G="\e[32m"
+N="\e[0m"
+Y="\e[33m"
+
+if [ $USERID -ne 0 ];
+then    
+    echo -e "$R ERROR:: Please run this scirpt with root access $N"
+    exit 1
+fi
+
+VALIDATE(){
+    if [ $1 -ne 0 ];
+    then
+        echo -e "$2 ... $R FAILURE $N"
+        exit 1
+    else
+        echo -e "$2 ... $G SUCCESS $N"
+    fi
+}    
+
+
+yum module disable mysql -y &>> $LOGFILE
+
+VALIDATE $? "Disabling default mysql version"
+
+cp /home/centos/roboshop-shell/mysql.repo /etc/yum.repos.d/mysql.repo &>> $LOGFILE
+
+VALIDATE $? "copying mysql repo"
+
+yum install mysql-community-server -y &>> $LOGFILE
+
+VALIDATE $? "Installing mysql server"
+
+systemctl enable mysqld &>> $LOGFILE
+
+VALIDATE $? "enabling mysql server"
+
+systemctl start mysqld &>> $LOGFILE
+
+VALIDATE $? "starting mysql server"
+
+mysql_secure_installation --set-root-pass RoboShop@1 &>> $LOGFILE
+
+VALIDATE $? "setting up mysql root password"
+
+
